b8f0f4a50dcb560a89718de8976ea94265f23e27
Karthikeyan Ilayakumar <karthikeyan.ilayakumar.us@gmail.com>
2023-09-01 14:00:59 -0500

    conditionalstatement.py

---
+++ conditionalstatement.py
@@ -0,0 +1,12 @@
+a = input("Enter A\n")
+
+if int(a) % 2 == 0:
+    print("Even\n")
+    a = 20
+    print("Printing a", a)
+elif int(a) % 2 != 0:
+    print("Odd\n")
+else:
+    print("This is not part of If statements")
+
+print("Even Number") if int(a) % 2 == 0 else print("Odd Number")
